2b15488c3bb646341b3190e00930ad57e54e8365
John Royce <royce@royce-iMac.local>
2020-10-06 17:44:57 +0800

    add plot pie

--- app/src/model.R
+++ app/src/model.R
@@ -2,6 +2,7 @@ library(mlbench)
 library(caret)
 library(doMC)
 library(tidyverse)
+library(plotrix)
 
 registerDoMC(cores=8)
 
@@ -76,6 +77,13 @@ plot_pairwise_xy <- function(data, models) {
     xyplot(data, models=models)
 }
 
+plot_pie <- function(x, y, title) {
+	colors = c('#4286f4','#bb3af2','#ed2f52','#efc023','#ea7441', '#000000')
+	piepercent<- round(x, 2)
+	pie3D(x, labels=piepercent, main=title, explode=0.1, height=0.05, col=colors)
+	legend("topright",y, cex = 0.8,fill = colors)
+}
+
 get_statistical_significance <- function(data) {
     diffs <- diff(data)
     summary(diffs)
@@ -109,6 +117,10 @@ index <- create_partition(DS$Class, 0.80, FALSE)
 trainData <- DS[index,]
 testData <- DS[-index,]
 
+x <- c()
+labels <- c()
+
+
 # # Logistic Regression
 run_lgr_model <- function (trainData, testData, metric, train_control) {
 	generate_seed(7)
@@ -121,11 +133,12 @@ run_lgr_model <- function (trainData, testData, metric, train_control) {
 	head(predictions)
 	return(model.lgr)
 }
-# # start.time <- Sys.time()
+labels <- c(labels, "Logistic Regression")
+start.time <- Sys.time()
 logistic_regression_model <- run_lgr_model(trainData, testData, METRIC, train_control)
-# # end.time <- Sys.time()
-# # time.taken <- end.time - start.time
-# # time.taken
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time, 2)
+x <- c(x, time.taken)
 
 # Random Forest 
 run_rf_model <- function (trainData, testData, metric, train_control) {
@@ -140,11 +153,13 @@ run_rf_model <- function (trainData, testData, metric, train_control) {
 	return(model.rf)
 }
 
-# # start.time <- Sys.time()
+
+labels <- c(labels, "Random Forest")
+start.time <- Sys.time()
 random_forest_model <- run_rf_model(trainData, testData, METRIC, train_control)
-# # end.time <- Sys.time()
-# # time.taken <- end.time - start.time
-# # time.taken
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time, 2)
+x <- c(x, time.taken)
 
 
 # Naive Bayes
@@ -159,11 +174,13 @@ run_nb_model <- function(trainData, testData, metric, train_control) {
 	return(model.nb)
 }
 
-# start.time <- Sys.time()
+labels <- c(labels, "Naive Bayes")
+start.time <- Sys.time()
 naive_bayes_model <- run_nb_model(trainData, testData, METRIC, train_control)
-# end.time <- Sys.time()
-# time.taken <- end.time - start.time
-# time.taken
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time, 2)
+x <- c(x, time.taken)
+
 
 #SVM
 run_svm_model <- function(trainData, testData, metric, train_control) {
@@ -179,36 +196,40 @@ run_svm_model <- function(trainData, testData, metric, train_control) {
 	return(model.svm)
 }
 
-# start.time <- Sys.time()
+labels <- c(labels, "Super Vector Machine")
+start.time <- Sys.time()
 super_vector_machine_model <- run_svm_model(trainData, testData, METRIC, train_control)
-# end.time <- Sys.time()
-# time.taken <- end.time - start.time
-# time.taken
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time, 2)
+x <- c(x, time.taken)
 
 
 #CART
 run_cart_model <- function(trainData, testData, metric, train_control) {
 	generate_seed(7)
 	model.cart <- train_model(trainData, "rpart", train_control, metric)
-	# model.cart
-	# summary(model.cart)
+	model.cart
+	summary(model.cart)
 	model.cart.final <- model.cart$finalModel
 	predictions <- make_predictions(model.cart, testData)
 	head(predictions)
 	confusionMatrix(predictions, testData$Class)
 	return(model.cart)
 }
-# start.time <- Sys.time()
+labels <- c(labels, "Classification and Regression Trees")
+start.time <- Sys.time()
 cart_model <- run_cart_model(trainData, testData, METRIC, train_control)
-# end.time <- Sys.time()
-# time.taken <- end.time - start.time
-# time.taken
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time, 2)
+x <- c(x, time.taken)
 
+
+#KNN
 run_knn_model <- function(trainData, testData, metric, train_control) {
 	generate_seed(7)
 	model.knn <- train_model(trainData, "knn", train_control, metric)
-	# model.knn
-	# summary(model.knn)
+	model.knn
+	summary(model.knn)
 	model.knn.final <- model.knn$finalModel
 	predictions <- make_predictions(model.knn, testData)
 	head(predictions)
@@ -216,23 +237,25 @@ run_knn_model <- function(trainData, testData, metric, train_control) {
 	return(model.knn)
 }
 
-# start.time <- Sys.time()
+labels <- c(labels, "K-Nearest Neighbor")
+start.time <- Sys.time()
 knn_model <- run_knn_model(trainData, testData, METRIC, train_control)
-# end.time <- Sys.time()
-# time.taken <- end.time - start.time
-# time.taken
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time, 2)
+x <- c(x, time.taken)
 
-results <- resamples(list(LGR=logistic_regression_model,RF=random_forest_model, NB=naive_bayes_model, SVM=super_vector_machine_model, CART=cart_model,KNN=knn_model))
-summary(results)
+# results <- resamples(list(LGR=logistic_regression_model,RF=random_forest_model, NB=naive_bayes_model, SVM=super_vector_machine_model, CART=cart_model,KNN=knn_model))
+# summary(results)
 
 
-scales <- list(x=list(relation="free"), y=list(relation="free"))
-# plot_bw(results, scales)
-# plot_density(results, scales)
-# plot_dot(results, scales)
+# scales <- list(x=list(relation="free"), y=list(relation="free"))
+# # plot_bw(results, scales)
+# # plot_density(results, scales)
+# # plot_dot(results, scales)
 
-# plot_parallel(results)
+# # plot_parallel(results)
 
-# plot_scatter_matrix(results)
+# # plot_scatter_matrix(results)
 
-plot_pairwise_xy(results, c=("LGR", "RF"))
+# plot_pairwise_xy(results, c=("LGR", "RF"))
+plot_pie(x, labels, "Time Execution")
